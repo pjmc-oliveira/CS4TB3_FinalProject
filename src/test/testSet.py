be8ab3bb4685b0286d00670d6ae3b38fb372216d
Piranaven Selva <piranaven.selva@hotmail.com>
2019-04-16 20:36:39 -0400

    Test cases outside of cpython

---
+++ src/test/testSet.py
@@ -0,0 +1,118 @@
+# Python test set -- part 1, grammar.
+# This just tests whether the parser accepts them all.
+
+# NOTE: When you run this test as a script from the command line, you
+# get warnings about certain hex/oct constants.  Since those are
+# issued by the parser, you can't suppress them by adding a
+# filterwarnings() call to this module.  Therefore, to shut up the
+# regression test, the filterwarnings() call has been added to
+# regrtest.py.
+
+from test.support import run_unittest, check_syntax_error
+import unittest
+import sys
+# testing import *
+from sys import *
+
+class UntilTests(unittest.TestCase):
+
+    def testFalseUntilStatment(self):
+        x = 0 
+        until x == 2:
+            x += 1 
+        self.assertEqual(x, 2)
+
+    def testTrueUntilStatment(self):
+        x = 2 
+        until x == 2:
+            x += 1 
+        self.assertEqual(x, 2)
+
+
+class UnlessTests(unittest.TestCase):
+
+    def testFalseUnlessTests(self):
+        x = 2
+        unless x == 2:
+            x -= 1
+        else:
+            x += 1
+        
+        self.assertEqual(x, 3)
+
+    def testTrueUnlessTests(self):
+        x = 2
+        unless x == 0:
+            x -= 1
+        else:
+            x += 1
+        
+        self.assertEqual(x, 1)
+        
+        
+
+class IncrementTests(unittest.TestCase):
+
+
+    def testIncrementTests(self):
+        x = 1
+        x++
+
+        self.assertEqual(x, 2)
+
+    def testNegativeIncrementTests(self):
+        x = -2
+        x++
+
+        self.assertEqual(x, -1)
+    
+    
+    def testFloatInrementTests(self):
+        x = 1.0
+        x++
+
+        self.assertEqual(x, 2.0)
+
+    def testPositiveFloatIncrementTests(self):
+        x = -1.0
+        x++
+        x++
+
+        self.assertEqual(x, 1.0)
+
+
+class DecrementTests(unittest.TestCase):
+
+
+    def testDecrementTests(self):
+        x = 1
+        x--
+
+        self.assertEqual(x, 0)
+
+
+    def testNegativeDecrementTests(self):
+        x = 0
+        x--
+
+        self.assertEqual(x, -1)
+    
+    
+    def testFloatDecrementTests(self):
+        x = 1.0
+        x--
+
+        self.assertEqual(x, 0.0)
+
+    def testNegativeFloatDecrementTests(self):
+        x = 0.0
+        x--
+
+        self.assertEqual(x, -1.0)
+
+
+def test_main():
+    run_unittest(UntilTests, UnlessTests, IncrementTests, DecrementTests)
+
+if __name__ == '__main__':
+    test_main()
